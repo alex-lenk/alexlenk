1734e5c7036797e8c556892cb51b486d3badb623
alex-lenk <alexlenk.ru@gmail.com>
2021-01-03 14:47:53 +0300

    Deleted old files

--- old-src/js/lib.js
+++
@@ -1,7 +0,0 @@
-//= ../../bower_components/jquery/dist/jquery.min.js
-
-//= lib/svgxuse.min.js
-
-// = lib/smoothscroll.js
-// = lib/bootstrap.min.js
-// = ../../bower_components/slick-carousel/slick/slick.min.js
