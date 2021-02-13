5480f9f1d8916709d09f833bacc7397a6325bef7
alexlenk.ru <alexlenk.ru@gmail.com>
2021-02-13 21:09:15 +0300

    Fixed temp

--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -7,30 +7,7 @@ import {Component, ViewEncapsulation} from "@angular/core";
 })
 
 export class HeaderComponent {
-  title = 'Alex Lenk'
+  title = 'Александр'
   logo = '../../assets/img/logo.svg'
   logoAlt = 'AlexLenk.ru HTML/CSS/JS coder'
-
-
-  OnClick() {
-    /* BEGIN: Плавное прокручивание к анкору ссылки */
-    /*let anchors = document.querySelectorAll('.js__go');
-    let anchor;
-
-    if (anchors.length) {
-      for (anchor of anchors) {
-        anchor.addEventListener('click', function (e) {
-          e.preventDefault();
-
-          const blockID = anchor.getAttribute('href').substr(1)
-
-          document.getElementById(blockID).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-          })
-        })
-      }
-    }*/
-    /* END:  */
-  }
 }
