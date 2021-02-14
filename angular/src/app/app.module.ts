a727e8b86ffeb9a99d0eaeb751a901fc221e6a84
alex-lenk <alexlenk.ru@gmail.com>
2021-02-14 18:43:51 +0300

    Created form validation on angular project

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
 
-import {AppComponent} from './app.component';
-import {FormsModule} from '@angular/forms';
-import {PostFormComponent} from './post-form/post-form.component';
-import {PostComponent} from './post/post.component';
-import {HeaderComponent} from './header/header.component';
-import {AppRoutingModule} from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { FooterComponent } from './include/footer/footer.component';
-import { HeaderPageComponent } from './include/header-page/header-page.component';
-import { MenuComponent } from './include/menu/menu.component';
-import { SkillsComponent } from './skills/skills.component';
-import { ContactsComponent } from './contacts/contacts.component';
+import {AppComponent} from './app.component'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {PostFormComponent} from './post-form/post-form.component'
+import {PostComponent} from './post/post.component'
+import {HeaderComponent} from './header/header.component'
+import {AppRoutingModule} from './app-routing.module'
+import { HomeComponent } from './home/home.component'
+import { ExperienceComponent } from './experience/experience.component'
+import { PortfolioComponent } from './portfolio/portfolio.component'
+import { FooterComponent } from './include/footer/footer.component'
+import { HeaderPageComponent } from './include/header-page/header-page.component'
+import { MenuComponent } from './include/menu/menu.component'
+import { SkillsComponent } from './skills/skills.component'
+import { ContactsComponent } from './contacts/contacts.component'
 
 @NgModule({
   declarations: [
@@ -34,7 +34,8 @@ import { ContactsComponent } from './contacts/contacts.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
